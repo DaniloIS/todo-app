fde9a950d89fc6a3f71e18529e1ff2d9e901b66b
DaniloIS <dailosantos-pg@hotmail.com>
2022-03-14 15:56:24 -0300

    Consulta e  Exclusão de TODOs

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import PageHeader from '../template/pageHeader';
 import TodoForm from './todoForm';
 import TodoList from './todoList';
+
+const URL = 'http://localhost:3003/api/todos';
 
 export default class Todo extends Component {
 
@@ -12,6 +16,14 @@ export default class Todo extends Component {
 
         this.handleChange = this.handleChange.bind(this);
         this.handleAdd = this.handleAdd.bind(this);
+        this.handleRemove = this.handleRemove.bind(this);
+
+        this.refresh();
+    }
+
+    refresh() {
+        axios.get(`${URL}?sort=-createdAt`)
+            .then(res => this.setState({...this.state, description: '', list: res.data}))
     }
 
     handleChange(e) {
@@ -19,7 +31,14 @@ export default class Todo extends Component {
     }
 
     handleAdd() {
-        
+        const description = this.state.description;
+        axios.post(URL, { description })
+            .then(res => this.refresh());
+    }
+
+    handleRemove(todo) {
+        axios.delete(`${URL}/${todo._id}`)
+            .then(res => this.refresh());
     }
 
     render() {
@@ -31,7 +50,10 @@ export default class Todo extends Component {
                     handleChange={this.handleChange}
                     handleAdd={this.handleAdd} 
                 />
-                <TodoList />
+                <TodoList 
+                    list={this.state.list} 
+                    handleRemove={this.handleRemove}
+                />
             </div>
         )
     }
