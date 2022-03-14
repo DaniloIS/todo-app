0a12da1c4e141ca90755d1c1334fdbe0f5b4e2b7
DaniloIS <danilosantos-pg@hotmail.com>
2022-03-14 02:15:19 -0300

    Habilitando o CORS

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,3 +1,5 @@
-const mongoose = require('mongoose');
+/*const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-module.exports = mongoose.connect('mongoose://localhost/todo');
+module.exports = mongoose.connect('mongoose://localhost/todo');*/
+
+const mongoose = require("mongoose"); mongoose.Promise = global.Promise; module.exports = mongoose.connect("mongodb://localhost:27017/todo", {     useMongoClient: true,     useNewUrlParser: true,     useUnifiedTopology: true, });
