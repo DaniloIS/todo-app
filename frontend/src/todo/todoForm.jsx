8aecb2cc44ef96e1cb21775f19dfd4aaa24c95d3
DaniloIS <danilosantos-pg@hotmail.com>
2022-03-16 01:44:17 -0300

    Melhorando a Action de Pesquisa

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -42,7 +42,7 @@ class TodoForm extends Component {
             </Grid>
             <Grid cols='12 3 2'>
                 <IconButton style='primary' icon='plus' onClick={() => add(description)} />
-                <IconButton style='info' icon='search' onClick={() => search()} />
+                <IconButton style='info' icon='search' onClick={search} />
                 <IconButton style='default' icon='close' onClick={this.props.clear} />
             </Grid>
         </div>
